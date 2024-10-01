0d3327fc020ed5c1379b013ee1f96f9b549c83f7
JoelArnaldich <joelarnaldich5@gmail.com>
2024-10-01 17:15:00 +0200

    a

---
+++ calculadora/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Display from './Display';
+import Keypad from './Keypad';
+import './styles/App.css';
+
+function App() {
+  const [input, setInput] = useState('');
+
+  const handleClick = (value) => {
+    if (value === 'C') {
+      setInput('');
+    } else if (value === '=') {
+      try {
+        setInput(eval(input).toString());
+      } catch (error) {
+        setInput('Error');
+      }
+    } else {
+      setInput(input + value);
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <Display input={input} />
+      <Keypad handleClick={handleClick} />
+    </div>
+  );
+}
+
+export default App;
